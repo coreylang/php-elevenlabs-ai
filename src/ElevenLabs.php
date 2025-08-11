d4bd0443fc918d7c9958900fb52190d7b27d4998
Corey Lang <coreylang.dev@gmail.com>
2025-08-11 01:11:58 -0500

    Pre-release 1.0.0-beta

---
+++ src/ElevenLabs.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ *  
+ * 
+ */
+
+namespace coreylang\ElevenLabsAI;
+
+trait Auth {
+
+    private string $apiKey;
+
+    public function SetAuthKey(?string $apiKey):void {
+        $this->apiKey = $apiKey;
+    }
+
+    private function GetAuthKey():string {
+        return $this->apiKey;
+    }
+}
+
+
+class ElevenLabs {
+    use Auth;
+}
