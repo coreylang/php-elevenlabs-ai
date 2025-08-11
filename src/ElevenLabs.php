064e9de3caafff387510c25804af2304df378cf4
Corey Lang <coreylang.dev@gmail.com>
2025-08-11 11:01:58 -0500

    Update paranthesis placement
    Updated class and method parantheses to
    match psr-12 rules.

--- src/ElevenLabs.php
+++ src/ElevenLabs.php
@@ -2,27 +2,24 @@
 
 declare(strict_types=1);
 
-/**
- *  
- * 
- */
-
 namespace coreylang\ElevenLabsAI;
 
-trait Auth {
-
+trait Auth
+{
     private string $apiKey;
 
-    public function SetAuthKey(?string $apiKey):void {
+    public function SetAuthKey(?string $apiKey):void
+    {
         $this->apiKey = $apiKey;
     }
 
-    private function GetAuthKey():string {
+    private function GetAuthKey():string
+    {
         return $this->apiKey;
     }
 }
 
-
-class ElevenLabs {
+class ElevenLabs
+{
     use Auth;
 }
