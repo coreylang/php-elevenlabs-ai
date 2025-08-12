0eb0611588d489b41fd72a54e9b54064e908658c
Corey Lang <coreylang.dev@gmail.com>
2025-08-12 15:55:01 -0500

    Update Twilio::MakeOutboundCall() response and args
    Added Elevenlabs::ReturnErrorMessage().
    
    Updated Twilio::MakeOutboundCall() response to return
    an object always.
    
    Added Twilio::MakeOutboundCall() new Conversation
    Initiation Client options.

--- src/ConversationalAI/Twilio.php
+++ src/ConversationalAI/Twilio.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace coreylang\ElevenLabsAI\ConversationalAI;
 
-use coreylang\ElevenLabsAI\ElevenLabs;
 use coreylang\ElevenLabsAI\Auth;
+use coreylang\ElevenLabsAI\ElevenLabs;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
@@ -32,6 +32,11 @@ class Twilio extends ElevenLabs
         //  [Initiation_First_Message] => optional, text, the agent greeting or first message
         //  [Initiation_Language] => optional, string, the language the agent speaks
         //  [Initiation_Voice_Id] => optional, string, the voice id of the agent
+        //  [INitiation_Native_MCP_Server_Ids] => optional, array, an array of MCP server ids
+        //  [Initiation_Custom_LLM_Extra_Body] => optional, string, the custom LLM extra body
+        //  [Initiation_User_Id] => optional, string, an user id for agent owner's user identification
+        //  [Initiation_Source] => optional, string, the application source name
+        //  [Initiation_Version] => optional, string, the application version
         //  [Initiation_Dynamic_Variables] => optional, array, an array of dynamic variables
         //  [
         //      'Variable_Name' => 'Variable_Value',
@@ -43,65 +48,86 @@ class Twilio extends ElevenLabs
         $agentPhoneNumberId = ($argsArray['Agent_Phone_Number_Id']) ?? "";
         $toPhoneNumber = ($argsArray['To_Phone_Number']) ?? "";
         if ($agentId === "")
-            throw "Agent_Id is missing.";
+            $response = $this->ReturnErrorResponse("Agent_Id is missing.");
         if ($agentPhoneNumberId === "")
-            throw "Phone_Number_Id is missing.";
+            $response = $this->ReturnErrorResponse("Phone_Number_Id is missing.");
         if ($toPhoneNumber === "")
-            throw "To_Phone_Number is missing.";
+            $response = $this->ReturnErrorResponse("To_Phone_Number is missing.");
 
         // optional
         $initiationPrompt = ($argsArray['Initiation_Prompt']) ?? "";
         $initiationFirstMessage = ($argsArray['Initiation_First_Message']) ?? "";
         $initiationLanguage = ($argsArray['Initiation_Language']) ?? "";
         $initiationVoiceId = ($argsArray['Intitiation_Voice_Id']) ?? "";
+        $initiationNativeMCPServerIds = ($argsArray['Initiation_Native_MCP_Server_Ids']) ?? [];
+        $initiationCustomLLMExtraBody = ($argsArray['Initiation_Custom_LLM_Extra_Body']) ?? "";
+        $initiationUserId = ($argsArray['Initiation_User_Id']) ?? "";
+        $initiationSource = ($argsArray['Initiation_Source']) ?? "";
+        $initiationVersion = ($argsArray['Initiation_Version']) ?? "";
         $initiationDynamicVariables = (!empty($argsArray['Initiation_Dynamic_Variables']))
                                         ? $argsArray['Initiation_Dynamic_Variables']
                                         : [];
+        if (!isset($response)) {
+            $payload = [];
+            $payload['agent_id'] = $agentId;
+            $payload['agent_phone_number_id'] = $agentPhoneNumberId;
+            $payload['to_number'] = $toPhoneNumber;
 
-        $headers = [
-            "Content-Type" => "application/json",
-            "xi-api-key" => $this->GetAuthKey()
-        ];
+            if (!empty($initiationFirstMessage)) {
+                $payload['conversation_initiation_client_data']['conversation_config_override']['agent']['first_message'] = $initiationFirstMessage;
+            }
 
-        $payload = [];
-        $payload['agent_id'] = $agentId;
-        $payload['agent_phone_number_id'] = $agentPhoneNumberId;
-        $payload['to_number'] = $toPhoneNumber;
+            if (!empty($initiationPrompt)) {
+                $payload['conversation_initiation_client_data']['conversation_config_override']['agent']['prompt']['prompt'] = $initiationPrompt;
+            }
 
-        if (!empty($initiationFirstMessage))
-        {
-            $payload['conversation_initiation_client_data']['conversation_config_override']['agent']['first_message'] = $initiationFirstMessage;
-        }
+            if (!empty($initiationLanguage)) {
+                $payload['conversation_initiation_client_data']['conversation_config_override']['agent']['language'] = $initiationLanguage;
+            }
 
-        if (!empty($initiationPrompt))
-        {
-            $payload['conversation_initiation_client_data']['conversation_config_override']['agent']['prompt']['prompt'] = $initiationPrompt;
-        }
+            if (!empty($initiationVoiceId)) {
+                $payload['conversation_initiation_client_data']['conversation_config_override']['tts']['voice_id'] = $initiationVoiceId;
+            }
 
-        if (!empty($initiationLanguage))
-        {
-            $payload['conversation_initiation_client_data']['conversation_config_override']['agent']['language'] = $initiationLanguage;
-        }
+            if (!empty($initiationNativeMCPServerIds)) {
+                $payload['conversation_initiation_client_data']['conversation_config_override']['agent']['prompt']['native_mcp_server_ids'] = $initiationNativeMCPServerIds;
+            }
 
-        if (!empty($initiationVoiceId))
-        {
-            $payload['conversation_initiation_client_data']['conversation_config_override']['tts']['voice_id'] = $initiationVoiceId;
-        }
+            if (!empty($initiationCustomLLMExtraBody)) {
+                $payload['conversation_initiation_client_data']['custom_llm_extra_body'] = $initiationCustomLLMExtraBody;
+            }
 
-        if (!empty($initiationDynamicVariables))
-        {
-            $payload['conversation_initiation_client_data']['dynamic_variables'] = $initiationDynamicVariables;
-        }
+            if (!empty($initiationUserId)) {
+                $payload['conversation_initiation_client_data']['user_id'] = $initiationUserId;
+            }
 
-        try {
-            $client = new Client();
-            $result = $client->post("https://api.us.elevenlabs.io/v1/convai/twilio/outbound_call", [
-                "json" => $payload,
-                "headers" => $headers
-            ]);
-            $response = $result->getBody()->getContents();
-        } catch(RequestException $e) {
-            throw $e->getMessage();
+            if (!empty($initiationSource)) {
+                $payload['conversation_initiation_client_data']['source_info']['source'] = $initiationSource;
+            }
+
+            if (!empty($initiationVersion)) {
+                $payload['conversation_initiation_client_data']['source_info']['version'] = $initiationVersion;
+            }
+
+            if (!empty($initiationDynamicVariables)) {
+                $payload['conversation_initiation_client_data']['dynamic_variables'] = $initiationDynamicVariables;
+            }
+
+            $headers = [
+                "Content-Type" => "application/json",
+                "xi-api-key" => $this->GetAuthKey()
+            ];
+
+            try {
+                $client = new Client();
+                $result = $client->post("https://api.us.elevenlabs.io/v1/convai/twilio/outbound_call", [
+                    "json" => $payload,
+                    "headers" => $headers
+                ]);
+                $response = json_decode($result->getBody()->getContents(), false);
+            } catch(RequestException $e) {
+                $response =  $e->getMessage();
+            }
         }
 
         return $response;
